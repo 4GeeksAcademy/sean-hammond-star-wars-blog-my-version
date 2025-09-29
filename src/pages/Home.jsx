903d241c91ed20bd99169a0c6c06b71a8a46fb65
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-09-29 20:36:34 +0000

    Started project and added new image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,32 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import React, { useEffect } from "react";
 
 export const Home = () => {
+  const baseURL = "https://www.swapi.tech/api";
 
-  const {store, dispatch} =useGlobalReducer()
+  const { store, dispatch } = useGlobalReducer();
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+  const getPeople = () => {
+    fetch(baseURL + "/people")
+      .then((allPeople) => {
+        return allPeople.json();
+      })
+      .then((data) => {
+        console.log("Star Wars people from SWAPI.tech API:", data.results);
+      });
+  };
+
+  useEffect(() => {
+    getPeople();
+  }, []);
+
+  return (
+    <div className="text-center mt-5">
+      <h1>My Star Wars Database</h1>
+      <p>
+		<img src={"https://upload.wikimedia.org/wikipedia/commons/c/ce/Star_wars2.svg"} />
+      </p>
+    </div>
+  );
+};
